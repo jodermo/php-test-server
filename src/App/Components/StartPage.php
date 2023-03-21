c8c6f898fa2bd09d8600a0a296f564b71d5ec1fd
Moritz Petzka <moritz@etzka.com>
2023-03-21 17:12:59 +0100

    new start page

--- src/App/Components/StartPage.php
+++ src/App/Components/StartPage.php
@@ -1,1 +1,8 @@
-<h3><?php echo $description ?></h3>
+<div class="start-page content-wrapper">
+    <div class="page-content">
+        <h3>Server Info</h3>
+        <div class="php-output">
+            <?php echo $app->arrayToHtmlTable($_SERVER); ?>
+        </div>
+    </div>
+</div>
